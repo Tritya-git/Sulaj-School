2c832aefc131989c69efb98416243aec0415a8e1
Tritya-git <76614642+Tritya-git@users.noreply.github.com>
2022-12-08 16:27:47 +0100

    upload

---
+++ 2022.12.08/2022.12.08/Program.cs
@@ -0,0 +1,35 @@
+﻿List<bool> börtön = new List<bool>();
+
+for (int i = 0; i < 100; i++)
+{
+    börtön.Add(true);
+}
+
+int num = 1;
+
+while (num < börtön.Count)
+{
+    for (int j = 0; j < börtön.Count; j++)
+    {
+        if (j % num == 0)
+        {
+            if (börtön[j] == true)
+            {
+                börtön[j] = false;
+            }
+            else
+            {
+                börtön[j] = true;
+            }
+        }
+    }
+    num++;
+}
+
+for (int i = 0; i < börtön.Count; i++)
+{
+    if (börtön[i] == false)
+    {
+        Console.WriteLine(i);
+    }
+}
